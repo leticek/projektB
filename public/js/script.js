09529c4c4ec8d9bee59c103d6a5052e5f8723f11
leticek <smiead@seznam.cz>
2020-12-06 22:44:41 +0100

    .

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,82 @@
+const calcTime = () => setInterval(() => {
+    var showDate = Date.parse("24 Dec 2020 08:00:00 GMT");
+    var currentTime = Date.now();
+    console.log(showDate.toLocaleString());
+    console.log(currentTime.toLocaleString());
+    var diff = showDate - currentTime;
+    var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    console.log(days);
+    console.log(hours);
+    console.log(minutes);
+    console.log(seconds);
+    document.getElementById('time-left').innerHTML = days + ':' + hours + ':' + minutes + ':' + seconds;
+
+}, 1000);
+
+var images = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const updateSlider = () => {
+    images = shuffle(images);
+    var indexes = [];
+    for(var i = 1; i < 5; i++){
+        
+        var element = document.querySelector("#gallery-image" + i + ">a");
+        var imageIndex = Math.floor(Math.random() * Math.floor(11));
+        while(indexes.includes(imageIndex)){
+            imageIndex = Math.floor(Math.random() * Math.floor(11));
+        }
+        indexes.push(imageIndex);
+        console.log(indexes);
+        var path = "./img/slider/" + indexes[i - 1] + ".jpg"
+        element.setAttribute("href", path);
+    }
+    
+}
+
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
+    var url = window.location.href;
+    if (url.includes("contact.html")) {
+        var mymap = L.map('myMap', {
+            dragging: false,
+            zoomControl: false,
+            scrollWheelZoom: false
+        }).setView([50.0562386, 14.536936], 17);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+        }).addTo(mymap);
 
-    var mymap = L.map('myMap', {
-        dragging: false,
-        zoomControl: false,
-        scrollWheelZoom: false
-    }).setView([50.0562386, 14.536936], 17);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-    }).addTo(mymap);
-
-    L.marker([50.0562386, 14.536936]).addTo(mymap);
-
-    console.log("haha");
+        L.marker([50.0562386, 14.536936]).addTo(mymap);
+        calcTime();
+    }
+    if (url.includes("index.html")) {
+        //setInterval(updateSlider, 3000);
+    }
 });
+
+
 
